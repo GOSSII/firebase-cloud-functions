939826d7bf57a06e07cb9d2569aeb77112c911f4
Dipesh Goswami <dipeshpuri9@gmail.com>
2019-07-09 09:42:04 -0400

    2 https calls

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -36,24 +36,76 @@ export const fetchSportsRadar = functions.https.onRequest((req, res) => {
     .catch(function (err: any) {
         res.send(err);
     });
+
+    const options2 = {
+        uri: "https://api.sportradar.us/nhl/production/v6/en/games/2017/09/26/schedule.json?api_key=p43cve7ruzvm62w63vym8vyf"
+    };
+    requestPromise.get(options2)
+    .then(function (body: any) {
+       // res.send(body);
+        // Save Sports Details into Firebase Realtime Database.
+        admin.database().ref('/sports/NHL').push(body)
+        .then(() => {
+             res.status(200).send(body)
+        }).catch(error => {
+            console.error(error);
+             res.status(500).send('Oh no! Error: ' + error);
+        });
+    })
+    .catch(function (err: any) {
+        res.send(err);
+    });
 })
 
 // Get Data From Firebase Realtime Database For Summery Function. & Push Data into Firestore.
-export const getSportsData = functions.https.onRequest((req, res,) => {
+export const summeryFunction = functions.https.onRequest((req, res,) => {
+    // Get Values FOr NBA
      admin.database().ref('/sports/NBA').on("value", (snapshot:any) => {
         //  console.log(snapshot.val());
         // return res.status(200).send(snapshot.val());
 
-
-
-        /*
-         Summery Cloud Function 
-        */
-
-
+            /*
+            Summery Cloud Function 
+               const gotObject = snapshot.val();
+               const postObject = {
+                //    "date": snapshot.val().league.name,
+                   "jj" : snapshot.val().date
+               }
+               console.log("Psot dart" + postObject);
+            */
 
         // Save Sports Details into Firebase FireStore Database.
         admin.firestore().doc('sports/NBA').set(snapshot.val())
+        .then(function() {
+            res.send("Document successfully written!");
+        })
+        .catch(function(error) {
+            res.send("Error writing document: "+ error);
+        });
+
+    }, (error: string) => {
+        console.error(error);
+         res.status(500).send('Oh no! Error: ' + error);
+    });
+
+
+    // Get Values FOr NHL
+    admin.database().ref('/sports/NHL').on("value", (snapshot:any) => {
+        //  console.log(snapshot.val());
+        // return res.status(200).send(snapshot.val());
+
+            /*
+            Summery Cloud Function 
+               const gotObject = snapshot.val();
+               const postObject = {
+                //    "date": snapshot.val().league.name,
+                   "jj" : snapshot.val().date
+               }
+               console.log("Psot dart" + postObject);
+            */
+
+        // Save Sports Details into Firebase FireStore Database.
+        admin.firestore().doc('sports/NHL').set(snapshot.val())
         .then(function() {
             res.send("Document successfully written!");
         })
